bdb20051fd4eb3e12829a8e8713a846bf47788fe
dahae <eg1377@naver.com>
2023-08-10 16:41:09 +0900

    [Fix]파일 이름변경
     중첩 오류 뜨는 파일이름 변경

--- client/src/pages/QuestionDescription.js
+++ client/src/pages/QuestionDescription.js
@@ -4,17 +4,17 @@
 // import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import React from 'react';
-import { LinkButton } from '../components/DescriptionComponents/Button/TagButton';
-import { AskButton } from '../components/DescriptionComponents/Button/AskButton';
-import BookButton from '../components/DescriptionComponents/Button/BookButton';
-import DownButton from '../components/DescriptionComponents/Button/DownButton';
-import UpButton from '../components/DescriptionComponents/Button/UpButton';
+import { LinkButton } from '../components/DescriptionComponents/ButtonComponents/TagButton';
+import { AskButton } from '../components/DescriptionComponents/ButtonComponents/AskButton';
+import BookButton from '../components/DescriptionComponents/ButtonComponents/BookButton';
+import DownButton from '../components/DescriptionComponents/ButtonComponents/DownButton';
 import './QuestionDescription.css';
 import UserInfoBox from '../components/DescriptionComponents/user_info/UserInfoBox';
-import BannerImg from '../components/DescriptionComponents/Banner/Banner';
-import ButtonList from '../components/DescriptionComponents/Button/Button';
-import { CommentBUT } from '../components/DescriptionComponents/Button/AddComment';
+import BannerImg from '../components/DescriptionComponents/Banner/BannerComponents';
+import ButtonList from '../components/DescriptionComponents/ButtonComponents/TextButton';
+import { CommentBUT } from '../components/DescriptionComponents/ButtonComponents/AddComment';
 import BalloonApp from '../components/Balloon/BalloonBox';
+import UpButton from '../components/DescriptionComponents/ButtonComponents/UpButton';
 
 const DescriptionBox = styled.div`
   display: flex;
