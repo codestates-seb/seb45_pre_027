c4aa059144248a534ae37e04cb0a4557568db5d6
hanbinchoi <wkghskak@naver.com>
2023-08-11 15:41:29 +0900

    Feat: 회원가입 페이지 구조 및 디자인

---
+++ client/src/components/signUp-components/SignUpForm.js
@@ -0,0 +1,205 @@
+import { styled } from "styled-components";
+import SocialButton from "../login-components/SocialButton";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { ErrorMessage } from "@hookform/error-message";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setIsLogin } from "../../store/loginSlice";
+import { setUserInfo } from "../../store/userInfoSlice";
+
+const Container = styled.div`
+  width: 100%;
+  padding: 24px 18px;
+  display: inline-flex;
+  align-items: flex-start;
+  border-radius: 8px;
+  background: #fff;
+  box-shadow:
+    0px 1px 4px 0px rgba(0, 0, 0, 0.1),
+    0px 20px 48px 0px rgba(0, 0, 0, 0.05),
+    0px 10px 24px 0px rgba(0, 0, 0, 0.05);
+`;
+const Form = styled.form`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 15px;
+  div {
+    width: 100%;
+
+    .password-helper {
+      padding-top: 4px;
+      color: #6a737c;
+      font-family: Inter;
+      font-size: 11px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 15.692px; /* 142.657% */
+    }
+  }
+  label {
+    color: #0c0d0e;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 19.615px; /* 140.11% */
+  }
+  input {
+    padding: 0.6em 0.7em;
+    width: 100%;
+    border-radius: 6px;
+    border: 1px solid #babfc4;
+    background: #fff;
+    font-weight: 300;
+
+    &:focus {
+      outline: none;
+      border-color: hsl(206, 90%, 69.5%);
+      box-shadow: 0px 0px 5px 1px hsl(206, 90%, 69.5%);
+    }
+  }
+`;
+const ErrorContainer = styled.div`
+  width: 100%;
+  padding-top: 0.5rem;
+  p {
+    color: red;
+    text-align: center;
+    font-size: 0.7rem;
+  }
+`;
+const LoginButton = styled(SocialButton)`
+  width: 100%;
+`;
+
+const HelperContainer = styled.div`
+  color: #6a737c;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 17px; /* 141.667% */
+
+  a {
+    color: #0074cc;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 17px;
+    list-style: none;
+    text-decoration: none;
+  }
+`;
+
+function SignUpForm() {
+  const isLogin = useSelector((state) => state.isLogin.value);
+  const userInfo = useSelector((state) => state.userInfo.value);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [errorMsg, setErrorMsg] = useState("");
+  const handleLogin = async (data) => {
+    await fetch(`${process.env.REACT_APP_SERVER_URL}/members/log-in/`, {
+      method: "POST",
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+    })
+      .then(async (res) => {
+        if (res.status === 200) {
+          sessionStorage.setItem("user_id", res.data);
+          dispatch(setIsLogin(res.data));
+          return await fetch(
+            `${process.env.REACT_APP_SERVER_URL}/members/${res.data}`
+          );
+        } else if (res.status === 403) {
+          setErrorMsg("로그인 실패");
+        }
+      })
+      .then((res) => {
+        dispatch(setUserInfo(res.data));
+        navigate("/");
+      });
+  };
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit(handleLogin)}>
+        <div>
+          <label>Email</label>
+          <input
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm,
+                message: "Please input a valid email format",
+              },
+            })}
+          />
+          <ErrorContainer>
+            <ErrorMessage
+              errors={errors}
+              name="email"
+              render={({ message }) => <p>{message}</p>}
+            />
+          </ErrorContainer>
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            {...register("password", {
+              required: "Password is required",
+              pattern: {
+                value:
+                  /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$/gm,
+                message: "Please input a valid password format",
+              },
+            })}
+            type="password"
+          />
+          <p className="password-helper">
+            Passwords must contain at least 8 characters, including at least 1
+            upper, 1 lower, 1 non-alpha and 1 number.
+          </p>
+          <ErrorContainer>
+            <ErrorMessage
+              errors={errors}
+              name="password"
+              render={({ message }) => <p>{message}</p>}
+            />
+          </ErrorContainer>
+        </div>
+
+        <HelperContainer>
+          <LoginButton
+            color="#FFF"
+            background="#0A95FF"
+            bordercolor="hsl(206,96%,90%)"
+            hovercolor="hsl(206,100%,40%)"
+            activecolor="hsl(209,100%,37.5%)"
+          >
+            Sign Up
+          </LoginButton>
+          <ErrorContainer>
+            <p>{errorMsg}</p>
+          </ErrorContainer>
+          By clicking "Sign up", you agree to our{" "}
+          <a href="#">terms of service</a> and acknowledge that you have read
+          and understand our <a href="#">privacy policy</a> and{" "}
+          <a href="#">code of conduct</a> .
+        </HelperContainer>
+      </Form>
+    </Container>
+  );
+}
+
+export default SignUpForm;
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,5 +1,229 @@
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+import tag from "../img/tag-icon.svg";
+import question from "../img/question-icon.svg";
+import trophy from "../img/trophy-icon.svg";
+import up from "../img/up-icon.svg";
+import SocialButton from "../components/login-components/SocialButton";
+import SmallLogo from "../components/login-components/SmallLogo";
+import facebookLogo from "../img/facebook-logo.svg";
+import googleLogo from "../img/google-logo.svg";
+import githubLogo from "../img/github-logo.svg";
+import SignUpForm from "../components/signUp-components/SignUpForm";
+import { BiLinkExternal } from "react-icons/bi";
+
+// 회원가입 페이지
+const Container = styled.div`
+  display: flex;
+  max-width: 1920px;
+  justify-content: center;
+  align-items: flex-start;
+  align-self: stretch;
+
+  background-color: rgba(241, 242, 243, 1);
+`;
+
+const InnerContainer = styled.div`
+  width: 785.33px;
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LeftContainer = styled.div`
+  width: 421.33px;
+  flex-shrink: 0;
+
+  h2 {
+    color: #232629;
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 27px; /* 108% */
+  }
+`;
+
+const LineContainer = styled.div`
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  align-self: center;
+  gap: 8px;
+
+  div {
+    display: flex;
+    padding-bottom: 0px;
+    justify-content: center;
+    align-items: flex-start;
+
+    img {
+      width: 26px;
+      height: 26px;
+    }
+  }
+
+  span {
+    color: #232629;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 19.615px; /* 140.11% */
+  }
+`;
+
+const HelperContainer = styled.div`
+  padding-top: 24px;
+  color: #6a737c;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 17px; /* 141.667% */
+
+  a {
+    color: #0074cc;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 17px;
+    list-style: none;
+    text-decoration: none;
+  }
+`;
+
+const RightContainer = styled.div`
+  width: 316px;
+  flex-shrink: 0;
+`;
+
+const ButtonContainer = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr;
+  gap: 8px;
+  padding-bottom: 20px;
+`;
+
+const BottomContainer = styled.div`
+  margin-top: 23px;
+  padding: 16px;
+  color: #232629;
+  text-align: center;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 17px; /* 141.667% */
+
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  a {
+    color: #0074cc;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 17px;
+    text-decoration: none;
+  }
+`;
+
 function SignUp() {
-  return <div>sign-up</div>;
+  return (
+    <Container>
+      <InnerContainer>
+        <LeftContainer>
+          <h2>Join the Stack Overflow community</h2>
+          <LineContainer>
+            <div>
+              <img src={question} alt="question"></img>{" "}
+            </div>
+            <span>Get unstuck — ask a question</span>
+          </LineContainer>
+          <LineContainer>
+            <div>
+              <img src={up} alt="up"></img>{" "}
+            </div>
+            <span>Unlock new privileges like voting and commenting</span>
+          </LineContainer>
+          <LineContainer>
+            <div>
+              <img src={tag} alt="tag"></img>{" "}
+            </div>
+            <span>
+              Save your favorite questions, answers, watch tags, and more
+            </span>
+          </LineContainer>
+          <LineContainer>
+            <div>
+              <img src={trophy} alt="trophy"></img>{" "}
+            </div>
+            <span>Earn reputation and badges</span>
+          </LineContainer>
+          <HelperContainer>
+            <p>
+              Collaborate and share knowledge with a private group for FREE.
+            </p>
+            <a href="#">
+              Get Stack Overflow for Teams free for up to 50 users.
+            </a>
+          </HelperContainer>
+        </LeftContainer>
+        <RightContainer>
+          <ButtonContainer>
+            <SocialButton
+              color="#3B4045"
+              background="#FFF"
+              bordercolor="#d6d9dc"
+              hovercolor="hsl(210, 8%, 97.5%)"
+              activecolor="hsl(210,8%,95%)"
+            >
+              <SmallLogo logo={googleLogo} />
+              <span>Log In with Google</span>
+            </SocialButton>
+            <SocialButton
+              color="#FFF"
+              background="#2F3337"
+              bordercolor="hsl(210,8%,85%)"
+              hovercolor="hsl(210,8%,15%)"
+              activecolor="hsl(210,8%,5%)"
+            >
+              <SmallLogo logo={githubLogo} />
+              <span>Log In with GitHub</span>
+            </SocialButton>
+            <SocialButton
+              color="#FFF"
+              background="#385499"
+              bordercolor="hsl(206,93%,83.5%)"
+              hovercolor="#314a86"
+              activecolor="#2a4074"
+              disabled
+            >
+              <SmallLogo logo={facebookLogo} />
+              <span>Log In with Facebook</span>
+            </SocialButton>
+          </ButtonContainer>
+          <SignUpForm />
+          <BottomContainer>
+            <div>
+              Already have an account? <Link to="/log-in">Log in</Link>
+            </div>
+            <div>
+              Are you an employer?{" "}
+              <a href="#">
+                Sign up on Talent <BiLinkExternal />
+              </a>
+            </div>
+          </BottomContainer>
+        </RightContainer>
+      </InnerContainer>
+    </Container>
+  );
 }
 
 export default SignUp;
