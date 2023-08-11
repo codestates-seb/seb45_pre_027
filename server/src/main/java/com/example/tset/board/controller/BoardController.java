876949eaa8e6b0065c71943ee027fbed778551e5
ChoHD <hduk1402@gmail.com>
2023-08-11 14:47:38 +0900

    Feat: "게시판 글 작성 기능 구현" 삭제 기능에서 soft 삭제, hard 삭제 기능 구현 2023.08.11

--- server/src/main/java/com/example/tset/board/controller/BoardController.java
+++ server/src/main/java/com/example/tset/board/controller/BoardController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -66,12 +67,29 @@ public class BoardController {
        return new ResponseEntity<>(
                new MultiResponseDto<>(boardMapper.boardsToBoardResponseDto(boards), pageBoards), HttpStatus.OK);
     }
-    @DeleteMapping("/{board-id}")
-    public ResponseEntity deleteBoard(@PathVariable("board-id") @Positive long boardId) {
+    @DeleteMapping("/{board-id}/soft")
+    public ResponseEntity softdeleteBoard(@PathVariable("board-id") @Positive long boardId) {
+        Board board = boardService.readBoard(boardId);
+
+        if (board == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        board.setDeletedAt(LocalDateTime.now());
+
+        Board deletedBoard = boardService.updateBoard(board);
+
+        if (deletedBoard == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping("/{board-id}/hard")
+    public ResponseEntity harddeleteBoard(@PathVariable("board-id") long boardId) {
         boardService.deleteBoard(boardId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
 
     @PostMapping("/{board-id}/click")
     public ResponseEntity<String> incrementView(@PathVariable("board-id") @Positive long boardId) {
