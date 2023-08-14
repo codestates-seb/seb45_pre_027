81e4e93b5a230149fa98a0fa71ad6288bc3c666d
dahae <eg1377@naver.com>
2023-08-14 14:44:24 +0900

    노드 삭제

--- client/src/components/DescriptionComponents/QuestionContent/QuestionContentBox.js
+++ client/src/components/DescriptionComponents/QuestionContent/QuestionContentBox.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 // 스타일 컴포넌트 (스타일을 원하면 추가/수정할 수 있습니다)
 const ContentContainer = styled.div`
   overflow-y: auto;
-  height: 300px; // 예시 높이입니다. 원하는 대로 조절 가능합니다.
+  height: 500px; // 예시 높이입니다. 원하는 대로 조절 가능합니다.
 `;
 
 const QuestionContent = () => {
