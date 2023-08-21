d17a12221af02ee9fc1da7c5068c73a3d2218ebc
ChoHD <hduk1402@gmail.com>
2023-08-22 00:35:39 +0900

    Feat : comment 기능 구현 완료 (H2에서 Test 완료)        Like, DisLike 기능 구현 완료 (H2에서 Test 완료)
    PreProject 백엔드 기능 구현
    
    2023.08.22

--- server/src/main/java/cs/pre/project/ProjectApplication.java
+++ server/src/main/java/cs/pre/project/ProjectApplication.java
@@ -6,13 +6,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @EnableJpaAuditing
-<<<<<<< HEAD:server/src/main/java/cs/pre/project/ProjectApplication.java
-public class ProjectApplication {
-=======
 @SpringBootApplication
-public class TestApplication {
-
->>>>>>> 5efa81fdc65dfbf5b0a618839bfa618a13414224:server/src/main/java/com/example/tset/TestApplication.java
+public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
